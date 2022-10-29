61d7f1aff82a535fc97cf782ddf8a9a7baec39a1
budy6991 <alvmenfer@gmail.com>
2022-10-30 02:03:26 +0300

    Changed aspect ratio of product cards

--- src/components.js/Card.js
+++ src/components.js/Card.js
@@ -2,11 +2,14 @@ import React from "react";
 
 export const Card = ({ title, price, image, id }) => {
   return (
-    <button className="w-1/6 m-6 border-spot-color border-2 text-spot-color rounded-xl flex flex-col justify-between items-center shadow-xl shadow-gray-800 transition-all hover:scale-105 hover:shadow-gray-300 hover:shadow-sm bg-neutral-900">
+    <button className="w-1/6 m-6  text-spot-color rounded-xl flex flex-col justify-between items-center shadow-xl shadow-gray-800 transition-all hover:scale-105 hover:shadow-gray-300 hover:shadow-sm bg-stone-800 hover:border-spot-color hover:border-2  ">
       <div className="p-6 ">
-        <img src={image}></img>
+        <img
+          className="h-fit w-fit object-fill aspect-square"
+          src={image}
+        ></img>
       </div>
-      <div className="text-center">
+      <div className="text-center font-main-title">
         <div>
           <h2>{title}</h2>
         </div>
