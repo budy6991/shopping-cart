d414629b524f154448b9be8e17a0987d6599f6cc
budy6991 <alvmenfer@gmail.com>
2022-10-30 01:45:51 +0300

    Added animation for card shadow

--- src/components.js/Card.js
+++ src/components.js/Card.js
@@ -2,12 +2,18 @@ import React from "react";
 
 export const Card = ({ title, price, image, id }) => {
   return (
-    <div className="w-1/6">
-      <div>
+    <button className="w-1/6 m-6 border-spot-color border-2 text-spot-color rounded-xl flex flex-col justify-between items-center shadow-xl shadow-gray-800 transition-all hover:scale-105 hover:shadow-gray-300 hover:shadow-sm bg-neutral-900">
+      <div className="p-6 ">
         <img src={image}></img>
       </div>
-      <h2>{title}</h2>
-      <h2>{price}</h2>
-    </div>
+      <div className="text-center">
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <div>
+          <h2>Price: {price}</h2>
+        </div>
+      </div>
+    </button>
   );
 };
